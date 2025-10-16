c336a0a751f9c6be9702b4b91b0bece0c2cc8789
maoudin <mathis.oudin42@gmail.com>
2025-10-16 15:17:46 +0200

    fix: fix button padding

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -15,7 +15,7 @@ export type ButtonActionProps = {
 export const Button: React.FC<ButtonProps> = ({buttonHref, buttonText}: ButtonProps): JSX.Element => {
     return (
         <Link href={buttonHref}
-              className="text-white px-5 py-3 max-w-fit bg-[#1b58f5] text-sm rounded-full flex justify-center items-center text-center capitalize hover:bg-[#1448d1] transition-colors duration-200">{buttonText}</Link>
+              className="text-white px-6 py-2 max-w-fit bg-[#1b58f5] text-sm rounded-full flex justify-center items-center text-center capitalize hover:bg-[#1448d1] transition-colors duration-200">{buttonText}</Link>
     )
 }
 
