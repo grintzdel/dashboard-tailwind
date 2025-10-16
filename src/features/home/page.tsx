b7ddff3efe6aba9919f720964038f5ad490d9321
maoudin <mathis.oudin42@gmail.com>
2025-10-16 14:18:23 +0200

    feat: add alert section

--- src/features/home/page.tsx
+++ src/features/home/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import AlertSection from "@/modules/app/react/alert-section";
 
 export default function HomePage() {
     return (
         <React.Fragment>
+            <AlertSection/>
         </React.Fragment>
     )
 }
